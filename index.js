cb0addb13422ae764f2c31cf9826169a6710f2ad
Mujahid <eng.mdmuzahidulislam@gmail.com>
2024-05-17 10:20:00 +0600

    connect the server with mongoDB

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const express = require('express')
 const app = express();
-const cors = require('cors')
+require('dotenv').config();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+
+// middlewares
 app.use(express.json())
 app.use(cors())
 
-app.get('/', async (req, res) => {
-    res.send("Cox's Perl is Running")
-})
+
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.userPass}@coxs-pearl.c2twe8z.mongodb.net/?retryWrites=true&w=majority&appName=coxs-pearl`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+const run = async () => {
+    try {
+
+        app.get('/', async (req, res) => {
+            res.send("Cox's Perl is Running")
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally { }
+}
+run().catch(console.dir);
 
 
 app.listen(port, async () => {
